995d9e40415b076dac2a6a4a06233c0db7c572ad
Geordie Rogers <geordie@grogers.xyz>
2018-04-30 22:38:57 +1200

    Customized parser
    Changed output file type, suppressed warnings, added copyright element

--- script/parser.py
+++ script/parser.py
@@ -2,20 +2,21 @@ import urllib2
 import json
 from bs4 import BeautifulSoup
 result = []
-file = open('output.txt','a')
+file = open('output.json','a')
 for x in xrange(1000, 7030): #You should able to run this in parallel, but I don't know how to do it
 	x = str(x)
 	try:
 		print "Fetching" + x + " ..."
 		response = urllib2.urlopen('https://earthview.withgoogle.com/' + x)
 		html = response.read()
-		html = BeautifulSoup(html)
+		html = BeautifulSoup(html, "html.parser")
 		Region = str((html.find("div", class_="content__location__region")).text.encode('utf-8'))
 		Country = str((html.find("div", class_="content__location__country")).text.encode('utf-8'))
+		Copyright = str((html.find("div", class_="content__attribution__text")).text.encode('utf-8'))[2:]
 		Everything = html.find("a", id="globe", href=True)
 		GMapsURL = Everything['href']
 		Image = 'https://www.gstatic.com/prettyearth/assets/full/' + x + '.jpg'
-		result.append({'region': Region, 'country': Country, 'map': GMapsURL, 'image': Image})
+		result.append({'id': x, 'region': Region, 'country': Country, 'copyright': Copyright, 'map': GMapsURL, 'image': Image})
 	
 	except urllib2.HTTPError, e:
 		continue #If the page is 404, then it will skip to the next one
